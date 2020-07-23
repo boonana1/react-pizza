473046fe0c615a08330b5a6981e7f19bb81de251
boonana1 <dima999366@gmail.com>
2020-07-23 19:51:17 +0500

    Uploading project

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import axios from 'axios';
+
+import { Header } from './components';
+import { Route } from 'react-router-dom';
+import { Home, Cart } from './pages';
+
+function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    axios.get('http://localhost:3000/db.json').then(({ data }) => {
+      setPizzas(data.pizzas);
+    });
+    //fetch('http://localhost:3000/db.json')
+    // .then((resp) => resp.json())
+    // .then((json) => {
+    //  setPizzas(json.pizzas);
+    // });
+  }, []);
+
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <Route path="/" render={() => <Home items={pizzas} />} exact />
+        <Route path="/cart" component={Cart} exact />
+      </div>
+    </div>
+  );
+}
+
+export default App;
